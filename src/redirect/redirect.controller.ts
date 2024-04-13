ac4c2f15e42bfad09dd592e30b43de26d602d5fe
Igor Nascimento <64155074+IgorNCS@users.noreply.github.com>
2024-04-13 16:16:32 -0300

    Feature: Add a count click when redirect by browser

--- src/redirect/redirect.controller.ts
+++ src/redirect/redirect.controller.ts
@@ -6,46 +6,14 @@ import { Request, Response } from 'express';
 export class RedirectController {
     constructor(private redirectService: RedirectService) { }
 
-    // @Get(':shortenedUrl')
-    // @Redirect('', 301)
-    // async redirectToOriginalUrl(@Param('shortenedUrl') shortenedUrl: string) {
-    //     const originalUrl = await this.redirectService.redirectToOriginalUrl(shortenedUrl);
-    //     console.log(originalUrl)
-    //     return { url: originalUrl };
-    // }
-
-    // @Get(':shortenedUrl')
-    // @Redirect('', 301)
-    // async redirectToOriginalUrl(@Param('shortenedUrl') shortenedUrl: string) {
-    //     const originalUrl = await this.redirectService.redirectToOriginalUrl(shortenedUrl);
-    //     return originalUrl;
-    // }
-
     @Get(':shortenedUrl')
     @Redirect('', 301)
-    async redirectToOriginalUrl(@Param('shortenedUrl') shortenedUrl: string) {
+    async redirectToOriginalUrl(@Param('shortenedUrl') shortenedUrl: string,@Res() res: Response) {
         const originalUrl = await this.redirectService.redirectToOriginalUrl(shortenedUrl);
         console.log(originalUrl)
+        res.setHeader('Cache-Control', 'no-store');
         return { url: `${originalUrl}` };
-        // return { url: originalUrl };
     }
 
-    // @Get('redirect/:redirectUrl')
-    // @Redirect(':redirectUrl', 301)
-    // async redirect(@Param('shortenedUrl') shortenedUrl: string) {
-    //     const originalUrl = await this.redirectService.redirectToOriginalUrl(shortenedUrl);
-    //     return { url: originalUrl };
-    // }
 
-    // @Get(':shortenedUrl')
-    // @Redirect('', 301)
-    // async redirect(@Param('shortenedUrl') shortenedUrl: string,@Res() res: Response, @Req() req: Request) {
-    //     try {
-    //         const originalUrl = await this.redirectService.redirectToOriginalUrl(shortenedUrl);
-    //         return { url: originalUrl };
-    //     } catch (error) {
-    //         return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Redirect Not Found', error: error });
-
-    //     }
-    // }
 }
